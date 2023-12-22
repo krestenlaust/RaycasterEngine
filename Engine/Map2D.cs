cb8e6107d181700d16076a795624db340523e6fe
Kresten Laust <krestenlaust@gmail.com>
2023-12-22 20:22:06 +0100

    Map type added

---
+++ Engine/Map2D.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine;
+
+public class Map2D
+{
+    public Position2D Cast(Position2D origin, Orientation2D direction, float maxDistance)
+    {
+        // Calculate offset to nearby grid.
+
+        // Extrapolate position by grid unit, till something is hit.
+
+        throw new NotImplementedException();
+    }
+}
