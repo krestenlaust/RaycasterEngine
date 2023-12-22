efa042e3449fd95f0e459767f6c37619915a509b
Kresten Laust <krestenlaust@gmail.com>
2023-12-22 20:36:27 +0100

    Basic implementation of the Engine library

---
+++ EngineImpl/Camera.cs
@@ -0,0 +1,9 @@
+﻿using Engine;
+
+namespace EngineImpl;
+
+public class Camera
+{
+    public Orientation2D Orientation { get; set; }
+    public Position2D Position { get; set; }
+}
---
+++ EngineImpl/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace EngineImpl;
+
+public class Game
+{
+    Camera player;
+
+    public Game()
+    {
+        player = new Camera();
+    }
+
+    public void Tick()
+    {
+        // Read input to reorient camera.
+    }
+}
