c448ab6394e2bec9a2c5e093e76436028d0aa9e1
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 13:05:48 +0100

    Extended Orientation-type

--- Engine/Orientation2D.cs
+++ Engine/Orientation2D.cs
@@ -1,6 +1,13 @@
 ﻿namespace Engine;
 
+/// <summary>
+/// Orientation based around the positive first-axis, going counterclockwise.
+/// </summary>
+/// <param name="radians"></param>
 public readonly struct Orientation2D(float radians)
 {
     public float Radians { get; } = radians;
+    public float Degrees { get; } = (180 / (float)Math.PI) * radians;
+    public bool Upperhalf { get; } = radians < Math.PI;
+    public bool Lefthalf { get; } = radians > (Math.PI / 2) && radians < ((Math.PI / 2) * 3);
 }
