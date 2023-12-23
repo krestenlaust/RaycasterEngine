0e2851cb9a82305ed5c412a802c39bb3d61352c9
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 21:59:37 +0100

    Extended Vector2D

--- Engine/Vector2D.cs
+++ Engine/Vector2D.cs
@@ -8,4 +8,6 @@ public readonly struct Vector2D(float x, float y)
     public float Length => (float)Math.Sqrt(X * X + Y * Y);
 
     public static Vector2D operator *(Vector2D vector, float factor) => new Vector2D(vector.X * factor, vector.Y * factor);
+
+    public Vector2D Floor => new Vector2D(MathF.Floor(X), MathF.Floor(Y));
 }
