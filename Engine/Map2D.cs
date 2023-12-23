18a74cac5beee8a608322a744f3a939283cd7a97
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 19:10:51 +0100

    Method bodies representing different .. methods of raycasting

--- Engine/Map2D.cs
+++ Engine/Map2D.cs
@@ -10,4 +10,27 @@ public class Map2D
 
         throw new NotImplementedException();
     }
+
+    /// <summary>
+    /// Casts using the basic incremental method.
+    /// TODO: Make each casting method have its own class via a interface.
+    /// This would make it easier to change raycast method to a non-euclidian method in the future.
+    /// </summary>
+    /// <returns></returns>
+    public Vector2D CastIncremental(Vector2D origin, Orientation2D direction, float maxDistance)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Finds the first hit along horizontal grid lines.
+    /// </summary>
+    /// <param name="origin"></param>
+    /// <param name="direction"></param>
+    /// <param name="maxDistance"></param>
+    /// <returns></returns>
+    Vector2D CastHorizontal(Vector2D origin, Orientation2D direction, float maxDistance)
+    {
+        throw new NotImplementedException();
+    }
 }
