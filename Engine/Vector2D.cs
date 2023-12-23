7f19b81b64d19431c7a7b89043dc267f5ff1ef8c
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 22:15:16 +0100

    Extended Vector2D with add operator

--- Engine/Vector2D.cs
+++ Engine/Vector2D.cs
@@ -14,6 +14,9 @@ public readonly struct Vector2D(float x, float y)
     public static Vector2D operator /(Vector2D vector, float divisor) =>
         new(vector.X / divisor, vector.Y / divisor);
 
+    public static Vector2D operator +(Vector2D vector1, Vector2D vector2) =>
+        new(vector1.X + vector2.X, vector1.Y + vector2.Y);
+
     public Vector2D Floor =>
         new(MathF.Floor(X), MathF.Floor(Y));
 
