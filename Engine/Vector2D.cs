ef70d4d468ccf4069c1c6a03fde42847dd9261d7
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 19:15:00 +0100

    Extended vector-type

--- Engine/Vector2D.cs
+++ Engine/Vector2D.cs
@@ -4,4 +4,8 @@ public readonly struct Vector2D(float x, float y)
 {
     public float X { get; } = x;
     public float Y { get; } = y;
+
+    public float Length => (float)Math.Sqrt(X * X + Y * Y);
+
+    public static Vector2D operator *(Vector2D vector, float factor) => new Vector2D(vector.X * factor, vector.Y * factor);
 }
