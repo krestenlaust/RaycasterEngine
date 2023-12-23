8231d29c9966cbdbd132db4f412c647bc6ec7d5b
Kresten Laust <krestenlaust@gmail.com>
2023-12-23 22:01:43 +0100

    MathF > (float)Math

--- Engine/Orientation2D.cs
+++ Engine/Orientation2D.cs
@@ -6,20 +6,20 @@
 /// <param name="radians"></param>
 public readonly struct Orientation2D(float radians)
 {
-    public static readonly Orientation2D FullRotation = new Orientation2D((float)Math.PI * 2);
+    public static readonly Orientation2D FullRotation = new Orientation2D(MathF.PI * 2);
     public static readonly Orientation2D NoRotation = new Orientation2D(0);
 
     public float Radians { get; } = radians;
     public Orientation2D Normalized { get => new Orientation2D(ExtendedMath.Modulo(Radians, FullRotation.Radians)); }
 
-    public float Degrees { get => (180 / (float)Math.PI) * Radians; }
+    public float Degrees { get => (180 / MathF.PI) * Radians; }
 
-    public bool Upperhalf { get => Normalized.Radians < Math.PI; }
+    public bool Upperhalf { get => Normalized.Radians < MathF.PI; }
 
     public bool Lefthalf { get => new Orientation2D(Radians - (FullRotation.Radians / 4)).Upperhalf; }
 
     public static Orientation2D FromDegrees(float degrees) =>
-        new Orientation2D(degrees * ((float)Math.PI / 180));
+        new Orientation2D(degrees * (MathF.PI / 180));
 
-    public Vector2D Vector { get => new Vector2D((float)Math.Cos(Radians), (float)Math.Sin(Radians)); }
+    public Vector2D Vector { get => new Vector2D(MathF.Cos(Radians), MathF.Sin(Radians)); }
 }
--- Engine/Vector2D.cs
+++ Engine/Vector2D.cs
@@ -5,7 +5,7 @@ public readonly struct Vector2D(float x, float y)
     public float X { get; } = x;
     public float Y { get; } = y;
 
-    public float Length => (float)Math.Sqrt(X * X + Y * Y);
+    public float Length => MathF.Sqrt(X * X + Y * Y);
 
     public static Vector2D operator *(Vector2D vector, float factor) => new Vector2D(vector.X * factor, vector.Y * factor);
 
