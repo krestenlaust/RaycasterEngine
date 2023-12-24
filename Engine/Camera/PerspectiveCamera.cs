422d4b9ce459761490771f6f4a52edeb6fe29f65
Kresten Laust <krestenlaust@gmail.com>
2023-12-25 00:28:02 +0100

    Removed redundant type-constraints

--- Engine/Camera/PerspectiveCamera.cs
+++ Engine/Camera/PerspectiveCamera.cs
@@ -2,9 +2,6 @@
 
 public class PerspectiveCamera<TCastMethod, TPosition, TOrientation, TLength, TRenderingUnit>
     where TCastMethod : ICast<TPosition, TOrientation, TLength, TRenderingUnit>
-    where TPosition : struct
-    where TOrientation : struct
-    where TLength : struct
 {
     public TOrientation FieldOfView { get; set; }
     public TPosition Position { get; set; }
--- Engine/ICast.cs
+++ Engine/ICast.cs
@@ -1,7 +1,6 @@
 ﻿namespace Engine;
 
 public interface ICast<TPosition, TOrientation, TLength, TRenderingUnit>
-    where TLength : struct
 {
     (TPosition, TRenderingUnit)? Cast(IMap<TPosition, TRenderingUnit> map, TPosition origin, TOrientation direction, TLength maxDistance);
 }
