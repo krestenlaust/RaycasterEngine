43c721c575567edb8eb098d83f0fddfd8c9d4a47
Kresten Laust <krestenlaust@gmail.com>
2023-12-25 00:59:25 +0100

    Added logic to PerspectiveCamera

--- Engine/Camera/PerspectiveCamera.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Engine.Camera;
-
-public class PerspectiveCamera<TCastMethod, TPosition, TOrientation, TLength, TRenderingUnit>
-    where TCastMethod : ICast<TPosition, TOrientation, TLength, TRenderingUnit>
-{
-    public TOrientation FieldOfView { get; set; }
-    public TPosition Position { get; set; }
-    public TOrientation Orientation { get; set; }
-}
