badd149b3182a511c5c207f836143ffda05b0464
Kresten Laust <krestenlaust@gmail.com>
2023-12-24 10:59:28 +0100

    Draft for camera

---
+++ Engine/Camera/PerspectiveCamera.cs
@@ -0,0 +1,11 @@
+﻿namespace Engine.Camera;
+
+public class PerspectiveCamera<TCastMethod, TPosition, TOrientation, TLength>
+    where TCastMethod : ICast<TPosition, TOrientation, TLength>
+    where TPosition : struct
+    where TOrientation : struct
+    where TLength : struct
+{
+    public TPosition Position { get; set; }
+    public TOrientation Orientation { get; set; }
+}
