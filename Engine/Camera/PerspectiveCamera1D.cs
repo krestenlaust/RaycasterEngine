3ed187ec1c16201d60984e267eda0c821c36fb4b
Kresten Laust <krestenlaust@gmail.com>
2023-12-25 11:09:08 +0100

    Added ICamera

--- Engine/Camera/PerspectiveCamera1D.cs
+++ Engine/Camera/PerspectiveCamera1D.cs
@@ -1,22 +1,20 @@
 ﻿namespace Engine.Camera;
 
-public class PerspectiveCamera1D<TCastMethod, TPosition, TLength, TRenderingUnit>(Orientation2D fieldOfView, TLength renderDistance)
+public class PerspectiveCamera1D<TCastMethod, TPosition, TLength, TRenderingUnit>(Orientation2D fieldOfView, int sampleSize) : ICamera<TCastMethod, TPosition, Orientation2D, TLength, TRenderingUnit>
     where TCastMethod : ICast<TPosition, Orientation2D, TLength, TRenderingUnit>
 {
     public Orientation2D FieldOfView { get; set; } = fieldOfView;
-    public TPosition Position { get; set; }
-    public Orientation2D Orientation { get; set; } = default;
-    public TLength RenderDistance { get; set; } = renderDistance;
+    public int SampleSize { get; set; } = sampleSize;
 
-    public IEnumerable<(TPosition, TRenderingUnit)?> Render(TCastMethod caster, IMap<TPosition, TRenderingUnit> map, int sampleSize)
+    public IEnumerable<(TPosition, TRenderingUnit)?> Render(IMap<TPosition, TRenderingUnit> map, TCastMethod caster, TPosition origin, Orientation2D orientation, TLength renderDistance)
     {
-        float startRadians = Orientation.Radians - (FieldOfView.Radians / 2);
-        float stepSizeRadians = FieldOfView.Radians / sampleSize;
+        float startRadians = orientation.Radians - (FieldOfView.Radians / 2);
+        float stepSizeRadians = FieldOfView.Radians / SampleSize;
 
-        for (int i = 0; i < sampleSize; i++)
+        for (int i = 0; i < SampleSize; i++)
         {
-            Orientation2D orientation = new Orientation2D(startRadians + stepSizeRadians * i);
-            yield return caster.Cast(map, Position, orientation, RenderDistance);
+            var currentDirection = new Orientation2D(startRadians + stepSizeRadians * i);
+            yield return caster.Cast(map, origin, currentDirection, renderDistance);
         }
     }
 }
---
+++ Engine/ICamera.cs
@@ -0,0 +1,7 @@
+﻿namespace Engine;
+
+public interface ICamera<TCastMethod, TPosition, in TAngle, in TLength, TRenderingUnit>
+    where TCastMethod : ICast<TPosition, TAngle, TLength, TRenderingUnit>
+{
+    IEnumerable<(TPosition, TRenderingUnit)?> Render(IMap<TPosition, TRenderingUnit> map, TCastMethod caster, TPosition origin, TAngle orientation, TLength renderDistance);
+}
