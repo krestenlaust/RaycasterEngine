0a0966eece2d0c1f510572a44d44cb3711892cc5
Kresten Laust <krestenlaust@gmail.com>
2023-12-25 01:00:39 +0100

    Draft for visual demo, no logic yet

---
+++ EngineImplForms/EngineImpl.cs
@@ -0,0 +1,18 @@
+﻿using Engine;
+using Engine.Camera;
+using Engine.CastMethod;
+
+namespace EngineImplForms;
+
+public class EngineImpl
+{
+    public void Setup()
+    {
+        var cam = new PerspectiveCamera1D<DiscreteRaycast<Color>, Vector2D, float, Color>(Orientation2D.FullRotation / 3, 10);
+    }
+
+    public void Render()
+    {
+
+    }
+}
