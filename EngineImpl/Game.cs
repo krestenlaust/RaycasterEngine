1206ece2c28b6b9dbc7a7568cfe9043b147338d1
Kresten Laust <krestenlaust@gmail.com>
2024-02-21 14:46:16 +0100

    Basic raycast impl

--- EngineImpl/Game.cs
+++ EngineImpl/Game.cs
@@ -1,12 +1,40 @@
-﻿namespace EngineImpl;
+﻿using Engine;
+using Engine.CameraPattern;
+using Engine.CastMethod;
+using Engine.MathTypes;
+
+namespace EngineImpl;
 
 public class Game
 {
-    Camera player;
+    Camera<DiscreteRaycast,
+           PerspectiveLineCameraPattern<
+               DiscreteRaycast,
+               Vector2D,
+               float>,
+           Vector2D,
+           Orientation2D,
+           float,
+           char> player;
 
     public Game()
     {
-        player = new Camera();
+        player =
+            new Camera<
+                DiscreteRaycast,
+                PerspectiveLineCameraPattern<
+                    DiscreteRaycast,
+                    Vector2D,
+                    float>,
+                Vector2D,
+                Orientation2D,
+                float,
+                char>
+                (new PerspectiveLineCameraPattern<
+                    DiscreteRaycast,
+                    Vector2D,
+                    float>
+                    (Orientation2D.FromDegrees(90), 50), new DiscreteRaycast(0.05f), 10);
     }
 
     public void Tick()
