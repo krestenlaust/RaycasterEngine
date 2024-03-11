6d60aff6e1f8ec963804b0aa835ff6945e2d580d
Kresten Laust <krestenlaust@gmail.com>
2024-03-11 15:12:39 +0100

    Basic progress

--- EngineImpl.Forms/FormCanvas.cs
+++ EngineImpl.Forms/FormCanvas.cs
@@ -3,6 +3,8 @@ namespace EngineImpl.Forms;
 public partial class FormCanvas : Form
 {
     Graphics graphics;
+    System.Windows.Forms.Timer gameLoopTimer;
+    Game game;
 
     public FormCanvas()
     {
@@ -12,5 +14,13 @@ public partial class FormCanvas : Form
     private void FormCanvas_Load(object sender, EventArgs e)
     {
         graphics = pictureBoxCanvas.CreateGraphics();
+        gameLoopTimer = new System.Windows.Forms.Timer();
+        gameLoopTimer.Tick += GameLoopTimer_Tick;
+    }
+
+    private void GameLoopTimer_Tick(object? sender, EventArgs e)
+    {
+
+        //game.Render()
     }
 }
