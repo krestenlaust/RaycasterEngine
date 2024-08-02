3f4e9e9e6a07db32215a95410f7cb1939acfbebb
Kresten Laust <krestenlaust@gmail.com>
2024-08-02 23:45:20 +0200

    Add properties to Spherical Direction-type

--- Engine/MathTypes/SphericalDirection.cs
+++ Engine/MathTypes/SphericalDirection.cs
@@ -4,6 +4,25 @@
 /// A kind of orientation-type.
 /// Direction described using two angles in a sphere, called polar and azimuthal.
 /// </summary>
-public readonly struct SphericalDirection(Angle inclination, Angle azimuth)
+public readonly struct SphericalDirection(Angle azimuthal, Angle polar)
 {
+    /// <summary>
+    /// Gets the azimuthal angle in the spherical direction.
+    /// Also referred to as roll.
+    /// </summary>
+    public Angle Azimuth { get; } = azimuthal;
+
+    /// <summary>
+    /// Gets the polar angle in the spherical direction.
+    /// Also referred to as pitch / tilt.
+    /// </summary>
+    public Angle Inclination { get; } = polar;
+
+    /// <summary>
+    /// Gets the cartesian directional unit-vector.
+    /// </summary>
+    public Vector3D Vector => new (
+        MathF.Sin(Azimuth.Radians) * MathF.Cos(Inclination.Radians),
+        MathF.Cos(Azimuth.Radians) * MathF.Cos(Inclination.Radians),
+        MathF.Sin(Inclination.Radians));
 }
