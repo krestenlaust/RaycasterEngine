296ca2e947e818ae5c4820d99515a950f8f5229d
Kresten Laust <krestenlaust@gmail.com>
2024-08-03 00:01:05 +0200

    Add basic convenience methods

--- Engine/MathTypes/Vector3D.cs
+++ Engine/MathTypes/Vector3D.cs
@@ -17,6 +17,30 @@ public readonly struct Vector3D(float x, float y, float z)
     /// </summary>
     public float Z { get; } = z;
 
+    /// <summary>
+    /// Gets the magnitude of the vector.
+    /// </summary>
+    public float Length =>
+        MathF.Sqrt((X * X) + (Y * Y) + (Z * Z));
+
+    /// <summary>
+    /// Gets the vector components as integers, according to <see cref="MathF.Floor(float)"/>.
+    /// </summary>
+    public Vector3D Floor =>
+        new (MathF.Floor(X), MathF.Floor(Y), MathF.Floor(Z));
+
+    /// <summary>
+    /// Gets the vector components as integers, according to <see cref="MathF.Round(float)"/>.
+    /// </summary>
+    public Vector3D Round =>
+        new (MathF.Round(X), MathF.Round(Y), MathF.Round(Z));
+
+    /// <summary>
+    /// Gets a normalized version.
+    /// </summary>
+    public Vector3D Normalized =>
+        this / Length;
+
     public static Vector3D operator *(Vector3D vector, float factor) =>
     new (vector.X * factor, vector.Y * factor, vector.Z * factor);
 
