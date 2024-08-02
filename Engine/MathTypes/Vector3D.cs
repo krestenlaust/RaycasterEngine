14d3a088606c8eaf628b5e48583bae9229d41d9e
Kresten Laust <krestenlaust@gmail.com>
2024-08-02 23:47:34 +0200

    Draft Vector3D-type

---
+++ Engine/MathTypes/Vector3D.cs
@@ -0,0 +1,19 @@
+﻿namespace Engine.MathTypes;
+
+public readonly struct Vector3D(float x, float y, float z)
+{
+    /// <summary>
+    /// Gets the ??? component of the vector-type.
+    /// </summary>
+    public float X { get; } = x;
+
+    /// <summary>
+    /// Gets the ??? component of the vector-type.
+    /// </summary>
+    public float Y { get; } = y;
+
+    /// <summary>
+    /// Gets the ??? component of the vector-type.
+    /// </summary>
+    public float Z { get; } = z;
+}
