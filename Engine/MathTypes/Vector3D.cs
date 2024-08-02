760b066ae1d8ad9bc11ff3a9d7f7283420161336
Kresten Laust <krestenlaust@gmail.com>
2024-08-03 00:00:25 +0200

    Implement operators

--- Engine/MathTypes/Vector3D.cs
+++ Engine/MathTypes/Vector3D.cs
@@ -16,4 +16,30 @@ public readonly struct Vector3D(float x, float y, float z)
     /// Gets the ??? component of the vector-type.
     /// </summary>
     public float Z { get; } = z;
+
+    public static Vector3D operator *(Vector3D vector, float factor) =>
+    new (vector.X * factor, vector.Y * factor, vector.Z * factor);
+
+    public static Vector3D operator /(Vector3D vector, float divisor) =>
+        new (vector.X / divisor, vector.Y / divisor, vector.Z / divisor);
+
+    public static Vector3D operator +(Vector3D vector1, Vector3D vector2) =>
+        new (vector1.X + vector2.X,
+            vector1.Y + vector2.Y,
+            vector1.Z + vector2.Z);
+
+    public static Vector3D operator -(Vector3D vector1, Vector3D vector2) =>
+        new (vector1.X - vector2.X,
+            vector1.Y - vector2.Y,
+            vector1.Z - vector2.Z);
+
+    public static bool operator ==(Vector3D vector1, Vector3D vector2) =>
+        vector1.X == vector2.X &&
+        vector1.Y == vector2.Y &&
+        vector1.Z == vector2.Z;
+
+    public static bool operator !=(Vector3D vector1, Vector3D vector2) =>
+        vector1.X != vector2.X ||
+        vector1.Y != vector2.Y ||
+        vector1.Z != vector2.Z;
 }
