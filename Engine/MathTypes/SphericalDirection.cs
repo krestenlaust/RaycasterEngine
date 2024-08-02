871e4d68eb1e8915b7f97f5c9c8a38d7e9c6fdaf
Kresten Laust <krestenlaust@gmail.com>
2024-08-02 22:50:42 +0200

    Draft type

---
+++ Engine/MathTypes/SphericalDirection.cs
@@ -0,0 +1,9 @@
+﻿namespace Engine.MathTypes;
+
+/// <summary>
+/// A kind of orientation-type.
+/// Direction described using two angles in a sphere, called polar and azimuthal.
+/// </summary>
+public readonly struct SphericalDirection(Angle inclination, Angle azimuth)
+{
+}
