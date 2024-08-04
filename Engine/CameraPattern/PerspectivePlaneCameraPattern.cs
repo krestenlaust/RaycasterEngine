2453711c792e7227ad35cf1dfb2274ab9a9b39c4
Kresten Laust <krestenlaust@gmail.com>
2024-08-04 20:45:53 +0200

    Finish perspective-plane camera

--- Engine/CameraPattern/PerspectivePlaneCameraPattern.cs
+++ Engine/CameraPattern/PerspectivePlaneCameraPattern.cs
@@ -2,19 +2,18 @@
 
 namespace Engine.CameraPattern;
 
-// TODO: The angle type needs to be spherical instead of planarly.
-public class PerspectivePlaneCameraPattern<TCastMethod, TPosition, TLength>(Angle fieldOfView, int sampleSize) : ICameraPattern<TCastMethod, TPosition, Angle, TLength>
-    where TCastMethod : ICastMethod<TPosition, Angle, TLength>
+public class PerspectivePlaneCameraPattern<TCastMethod, TPosition, TLength>(Angle fieldOfViewVertical, Angle fieldOfViewHorizontal, int sampleSize) : ICameraPattern<TCastMethod, TPosition, SphericalDirection, TLength>
+    where TCastMethod : ICastMethod<TPosition, SphericalDirection, TLength>
 {
     /// <summary>
     /// Gets or sets the vertical field of view, of the camera pattern.
     /// </summary>
-    public Angle FieldOfViewVertical { get; set; } = fieldOfView;
+    public Angle FieldOfViewVertical { get; set; } = fieldOfViewVertical;
 
     /// <summary>
     /// Gets or sets the horizontal field of view, of the camera pattern.
     /// </summary>
-    public Angle FieldOfViewHorizontal { get; set; } = fieldOfView;
+    public Angle FieldOfViewHorizontal { get; set; } = fieldOfViewHorizontal;
 
     /// <summary>
     /// Gets or sets the amount of vertical samples to take within the field of view.
@@ -26,19 +25,23 @@ public class PerspectivePlaneCameraPattern<TCastMethod, TPosition, TLength>(Angl
     /// </summary>
     public int SampleSizeHorizontal { get; set; } = sampleSize;
 
-    public IEnumerable<Hit<TPosition, TLength>?> Render(IHitSpace<TPosition> space, TCastMethod caster, TPosition origin, Angle orientation, TLength renderDistance)
+    public IEnumerable<Hit<TPosition, TLength>?> Render(IHitSpace<TPosition> space, TCastMethod caster, TPosition origin, SphericalDirection orientation, TLength renderDistance)
     {
-        float startRadiansVertical = orientation.Radians - (FieldOfViewVertical.Radians / 2);
+        float startRadiansVertical = orientation.Inclination.Radians - (FieldOfViewVertical.Radians / 2);
         float stepSizeRadiansVertical = FieldOfViewVertical.Radians / SampleSizeVertical;
 
         for (int y = 0; y < SampleSizeVertical; y++)
         {
-            float startRadiansHorizontal = orientation.Radians - (FieldOfViewHorizontal.Radians / 2);
+            Angle polarAngle = new (startRadiansVertical + (stepSizeRadiansVertical * y));
+
+            float startRadiansHorizontal = orientation.Azimuth.Radians - (FieldOfViewHorizontal.Radians / 2);
             float stepSizeRadiansHorizontal = FieldOfViewHorizontal.Radians / SampleSizeHorizontal;
 
             for (int x = 0; x < SampleSizeHorizontal; x++)
             {
-                var currentDirection = new Angle(startRadiansHorizontal + (stepSizeRadiansHorizontal * x));
+                Angle azimuthalAngle = new (startRadiansHorizontal + (stepSizeRadiansHorizontal * x));
+
+                SphericalDirection currentDirection = new (azimuthalAngle, polarAngle);
 
                 if (caster.Cast(space, origin, currentDirection, renderDistance, out Hit<TPosition, TLength>? hit))
                 {
