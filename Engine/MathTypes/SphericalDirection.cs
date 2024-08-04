e277ac59d060ffe0c988824e3774ee2233932b42
Kresten Laust <krestenlaust@gmail.com>
2024-08-04 17:33:58 +0200

    Implement Equatable

--- Engine/MathTypes/SphericalDirection.cs
+++ Engine/MathTypes/SphericalDirection.cs
@@ -4,7 +4,7 @@
 /// A kind of orientation-type.
 /// Direction described using two angles in a sphere, called polar and azimuthal.
 /// </summary>
-public readonly struct SphericalDirection(Angle azimuthal, Angle polar)
+public readonly struct SphericalDirection(Angle azimuthal, Angle polar) : IEquatable<SphericalDirection>
 {
     /// <summary>
     /// Gets the azimuthal angle in the spherical direction.
@@ -26,10 +26,23 @@ public readonly struct SphericalDirection(Angle azimuthal, Angle polar)
         MathF.Cos(Azimuth.Radians) * MathF.Cos(Inclination.Radians),
         MathF.Sin(Inclination.Radians));
 
+    public static bool operator ==(SphericalDirection left, SphericalDirection right) => left.Equals(right);
+
+    public static bool operator !=(SphericalDirection left, SphericalDirection right) => !(left == right);
+
     /// <summary>
     /// Converts a vector (unit or otherwise) into the spherical direction representation.
     /// </summary>
     /// <param name="vector"></param>
     /// <returns></returns>
     public static SphericalDirection FromVector(Vector3D vector) => throw new NotImplementedException();
+
+    /// <inheritdoc/>
+    public bool Equals(SphericalDirection other) => Azimuth.Equals(other.Azimuth) && Inclination.Equals(other.Inclination);
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj) => obj is SphericalDirection direction && Equals(direction);
+
+    /// <inheritdoc/>
+    public override int GetHashCode() => HashCode.Combine(Azimuth, Inclination);
 }
